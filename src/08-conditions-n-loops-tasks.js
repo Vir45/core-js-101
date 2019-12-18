9234de5c0d9f08c669fb5926b482005363e34ea5
Vova <vir42@mail.ru>
2019-12-18 21:30:08 +0300

    dfd

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -27,8 +27,16 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  let result = num;
+  if (num % 3 === 0 && num % 5 === 0) {
+    result = 'FizzBuzz';
+  } else if (num % 3 === 0) {
+    result = 'Fizz';
+  } else if (num % 5 === 0) {
+    result = 'Buzz';
+  }
+  return result;
 }
 
 
@@ -43,8 +51,15 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i += 1) {
+    result *= i;
+  }
+  return result;
 }
 
 
@@ -60,8 +75,12 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let result = n1;
+  for (let i = n1 + 1; i <= n2; i += 1) {
+    result += i;
+  }
+  return result;
 }
 
 
@@ -80,8 +99,11 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if (a < b + c && a > b - c && b < a + c && b > a - c && c < a + b && c > a - b) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -117,8 +139,22 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  let result = false;
+  const top1 = rect1.top + rect1.width;
+  const left1 = rect1.left + rect1.height;
+  const top2 = rect2.top + rect2.width;
+  const left2 = rect2.left + rect2.height;
+  if (top2 >= top1 && rect2.top <= top1 && left2 >= left1 && rect2.left <= left1) {
+    result = true;
+  } else if (top1 >= top2 && rect1.top <= top2 && left1 >= left2 && rect1.left <= left2) {
+    result = true;
+  } else if (top2 >= top1 && rect2.top <= top1 && left2 <= left1 && left2 >= rect1.left) {
+    result = true;
+  } else if (top1 >= top2 && rect1.top <= top2 && left1 <= left2 && left1 >= rect2.left) {
+    result = true;
+  }
+  return result;
 }
 
 
@@ -148,255 +184,260 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const y = Math.abs(circle.center.y) - point.y;
+  const x = Math.abs(circle.center.x) - point.x;
+  if (Math.sqrt(y * y + x * x) < circle.radius) {
+    return true;
+  }
+  return false;
 }
 
 
-/**
- * Returns the first non repeated char in the specified strings otherwise returns null.
- *
- * @param {string} str
- * @return {string}
- *
- * @example:
- *   'The quick brown fox jumps over the lazy dog' => 'T'
- *   'abracadabra'  => 'c'
- *   'entente' => null
- */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Returns the first non repeated char in the specified strings otherwise returns null.
+//  *
+//  * @param {string} str
+//  * @return {string}
+//  *
+//  * @example:
+//  *   'The quick brown fox jumps over the lazy dog' => 'T'
+//  *   'abracadabra'  => 'c'
+//  *   'entente' => null
+//  */
+// function findFirstSingleChar(str) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Returns the string representation of math interval,
- * specified by two points and include / exclude flags.
- * See the details: https://en.wikipedia.org/wiki/Interval_(mathematics)
- *
- * Please take attention, that the smaller number should be the first in the notation
- *
- * @param {number} a
- * @param {number} b
- * @param {bool} isStartIncluded
- * @param {bool} isEndIncluded
- * @return {string}
- *
- * @example
- *   0, 1, true, true   => '[0, 1]'
- *   0, 1, true, false  => '[0, 1)'
- *   0, 1, false, true  => '(0, 1]'
- *   0, 1, false, false => '(0, 1)'
- * Smaller number has to be first :
- *   5, 3, true, true   => '[3, 5]'
- *
- */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Returns the string representation of math interval,
+//  * specified by two points and include / exclude flags.
+//  * See the details: https://en.wikipedia.org/wiki/Interval_(mathematics)
+//  *
+//  * Please take attention, that the smaller number should be the first in the notation
+//  *
+//  * @param {number} a
+//  * @param {number} b
+//  * @param {bool} isStartIncluded
+//  * @param {bool} isEndIncluded
+//  * @return {string}
+//  *
+//  * @example
+//  *   0, 1, true, true   => '[0, 1]'
+//  *   0, 1, true, false  => '[0, 1)'
+//  *   0, 1, false, true  => '(0, 1]'
+//  *   0, 1, false, false => '(0, 1)'
+//  * Smaller number has to be first :
+//  *   5, 3, true, true   => '[3, 5]'
+//  *
+//  */
+// function getIntervalString( /* a, b, isStartIncluded, isEndIncluded */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Reverse the specified string (put all chars in reverse order)
- *
- * @param {string} str
- * @return {string}
- *
- * @example:
- * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
- * 'abracadabra' => 'arbadacarba'
- * 'rotator' => 'rotator'
- * 'noon' => 'noon'
- */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Reverse the specified string (put all chars in reverse order)
+//  *
+//  * @param {string} str
+//  * @return {string}
+//  *
+//  * @example:
+//  * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+//  * 'abracadabra' => 'arbadacarba'
+//  * 'rotator' => 'rotator'
+//  * 'noon' => 'noon'
+//  */
+// function reverseString( /* str */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Reverse the specified integer number (put all digits in reverse order)
- *
- * @param {number} num
- * @return {number}
- *
- * @example:
- *   12345 => 54321
- *   1111  => 1111
- *   87354 => 45378
- *   34143 => 34143
- */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Reverse the specified integer number (put all digits in reverse order)
+//  *
+//  * @param {number} num
+//  * @return {number}
+//  *
+//  * @example:
+//  *   12345 => 54321
+//  *   1111  => 1111
+//  *   87354 => 45378
+//  *   34143 => 34143
+//  */
+// function reverseInteger( /* num */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Validates the CCN (credit card number) and return true if CCN is valid
- * and false otherwise.
- *
- * See algorithm here : https://en.wikipedia.org/wiki/Luhn_algorithm
- *
- * @param {number} cnn
- * @return {boolean}
- *
- * @example:
- *   79927398713      => true
- *   4012888888881881 => true
- *   5123456789012346 => true
- *   378282246310005  => true
- *   371449635398431  => true
- *
- *   4571234567890111 => false
- *   5436468789016589 => false
- *   4916123456789012 => false
- */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Validates the CCN (credit card number) and return true if CCN is valid
+//  * and false otherwise.
+//  *
+//  * See algorithm here : https://en.wikipedia.org/wiki/Luhn_algorithm
+//  *
+//  * @param {number} cnn
+//  * @return {boolean}
+//  *
+//  * @example:
+//  *   79927398713      => true
+//  *   4012888888881881 => true
+//  *   5123456789012346 => true
+//  *   378282246310005  => true
+//  *   371449635398431  => true
+//  *
+//  *   4571234567890111 => false
+//  *   5436468789016589 => false
+//  *   4916123456789012 => false
+//  */
+// function isCreditCardNumber( /* ccn */ ) {
+//   throw new Error('Not implemented');
+// }
 
-/**
- * Returns the digital root of integer:
- *   step1 : find sum of all digits
- *   step2 : if sum > 9 then goto step1 otherwise return the sum
- *
- * @param {number} n
- * @return {number}
- *
- * @example:
- *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
- *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
- *   10000 ( 1+0+0+0+0 = 1 ) => 1
- *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
- */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
-}
-
-
-/**
- * Returns true if the specified string has the balanced brackets and false otherwise.
- * Balanced means that is, whether it consists entirely of pairs of opening/closing brackets
- * (in that order), none of which mis-nest.
- * Brackets include [],(),{},<>
- *
- * @param {string} str
- * @return {boolean}
- *
- * @example:
- *   '' => true
- *   '[]'  => true
- *   '{}'  => true
- *   '()   => true
- *   '[[]' => false
- *   ']['  => false
- *   '[[][][[]]]' => true
- *   '[[][]][' => false
- *   '{)' = false
- *   '{[(<{[]}>)]}' = true
- */
-function isBracketsBalanced(/* str */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Returns the digital root of integer:
+//  *   step1 : find sum of all digits
+//  *   step2 : if sum > 9 then goto step1 otherwise return the sum
+//  *
+//  * @param {number} n
+//  * @return {number}
+//  *
+//  * @example:
+//  *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+//  *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+//  *   10000 ( 1+0+0+0+0 = 1 ) => 1
+//  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
+//  */
+// function getDigitalRoot( /* num */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Returns the string with n-ary (binary, ternary, etc, where n <= 10)
- * representation of specified number.
- * See more about
- * https://en.wikipedia.org/wiki/Binary_number
- * https://en.wikipedia.org/wiki/Ternary_numeral_system
- * https://en.wikipedia.org/wiki/Radix
- *
- * @param {number} num
- * @param {number} n, radix of the result
- * @return {string}
- *
- * @example:
- *   1024, 2  => '10000000000'
- *   6561, 3  => '100000000'
- *    365, 2  => '101101101'
- *    365, 3  => '111112'
- *    365, 4  => '11231'
- *    365, 10 => '365'
- */
-function toNaryString(/* num, n */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Returns true if the specified string has the balanced brackets and false otherwise.
+//  * Balanced means that is, whether it consists entirely of pairs of opening/closing brackets
+//  * (in that order), none of which mis-nest.
+//  * Brackets include [],(),{},<>
+//  *
+//  * @param {string} str
+//  * @return {boolean}
+//  *
+//  * @example:
+//  *   '' => true
+//  *   '[]'  => true
+//  *   '{}'  => true
+//  *   '()   => true
+//  *   '[[]' => false
+//  *   ']['  => false
+//  *   '[[][][[]]]' => true
+//  *   '[[][]][' => false
+//  *   '{)' = false
+//  *   '{[(<{[]}>)]}' = true
+//  */
+// function isBracketsBalanced( /* str */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Returns the commom directory path for specified array of full filenames.
- *
- * @param {array} pathes
- * @return {string}
- *
- * @example:
- *   ['/web/images/image1.png', '/web/images/image2.png']  => '/web/images/'
- *   ['/web/assets/style.css', '/web/scripts/app.js',  'home/setting.conf'] => ''
- *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
- *   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
- */
-function getCommonDirectoryPath(/* pathes */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Returns the string with n-ary (binary, ternary, etc, where n <= 10)
+//  * representation of specified number.
+//  * See more about
+//  * https://en.wikipedia.org/wiki/Binary_number
+//  * https://en.wikipedia.org/wiki/Ternary_numeral_system
+//  * https://en.wikipedia.org/wiki/Radix
+//  *
+//  * @param {number} num
+//  * @param {number} n, radix of the result
+//  * @return {string}
+//  *
+//  * @example:
+//  *   1024, 2  => '10000000000'
+//  *   6561, 3  => '100000000'
+//  *    365, 2  => '101101101'
+//  *    365, 3  => '111112'
+//  *    365, 4  => '11231'
+//  *    365, 10 => '365'
+//  */
+// function toNaryString( /* num, n */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Returns the product of two specified matrixes.
- * See details: https://en.wikipedia.org/wiki/Matrix_multiplication
- *
- * @param {array} m1
- * @param {array} m2
- * @return {array}
- *
- * @example:
- *   [[ 1, 0, 0 ],       [[ 1, 2, 3 ],           [[ 1, 2, 3 ],
- *    [ 0, 1, 0 ],   X    [ 4, 5, 6 ],     =>     [ 4, 5, 6 ],
- *    [ 0, 0, 1 ]]        [ 7, 8, 9 ]]            [ 7, 8, 9 ]]
- *
- *                        [[ 4 ],
- *   [[ 1, 2, 3]]    X     [ 5 ],          =>     [[ 32 ]]
- *                         [ 6 ]]
- *
- */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Returns the commom directory path for specified array of full filenames.
+//  *
+//  * @param {array} pathes
+//  * @return {string}
+//  *
+//  * @example:
+//  *   ['/web/images/image1.png', '/web/images/image2.png']  => '/web/images/'
+//  *   ['/web/assets/style.css', '/web/scripts/app.js',  'home/setting.conf'] => ''
+//  *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
+//  *   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
+//  */
+// function getCommonDirectoryPath( /* pathes */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
-/**
- * Returns the evaluation of the specified tic-tac-toe position.
- * See the details: https://en.wikipedia.org/wiki/Tic-tac-toe
- *
- * Position is provides as 3x3 array with the following values: 'X','0', undefined
- * Function should return who is winner in the current position according to the game rules.
- * The result can be: 'X','0',undefined
- *
- * @param {array} position
- * @return {string}
- *
- * @example
- *
- *   [[ 'X',   ,'0' ],
- *    [    ,'X','0' ],       =>  'X'
- *    [    ,   ,'X' ]]
- *
- *   [[ '0','0','0' ],
- *    [    ,'X',    ],       =>  '0'
- *    [ 'X',   ,'X' ]]
- *
- *   [[ '0','X','0' ],
- *    [    ,'X',    ],       =>  undefined
- *    [ 'X','0','X' ]]
- *
- *   [[    ,   ,    ],
- *    [    ,   ,    ],       =>  undefined
- *    [    ,   ,    ]]
- *
- */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
-}
+// /**
+//  * Returns the product of two specified matrixes.
+//  * See details: https://en.wikipedia.org/wiki/Matrix_multiplication
+//  *
+//  * @param {array} m1
+//  * @param {array} m2
+//  * @return {array}
+//  *
+//  * @example:
+//  *   [[ 1, 0, 0 ],       [[ 1, 2, 3 ],           [[ 1, 2, 3 ],
+//  *    [ 0, 1, 0 ],   X    [ 4, 5, 6 ],     =>     [ 4, 5, 6 ],
+//  *    [ 0, 0, 1 ]]        [ 7, 8, 9 ]]            [ 7, 8, 9 ]]
+//  *
+//  *                        [[ 4 ],
+//  *   [[ 1, 2, 3]]    X     [ 5 ],          =>     [[ 32 ]]
+//  *                         [ 6 ]]
+//  *
+//  */
+// function getMatrixProduct( /* m1, m2 */ ) {
+//   throw new Error('Not implemented');
+// }
+
+
+// /**
+//  * Returns the evaluation of the specified tic-tac-toe position.
+//  * See the details: https://en.wikipedia.org/wiki/Tic-tac-toe
+//  *
+//  * Position is provides as 3x3 array with the following values: 'X','0', undefined
+//  * Function should return who is winner in the current position according to the game rules.
+//  * The result can be: 'X','0',undefined
+//  *
+//  * @param {array} position
+//  * @return {string}
+//  *
+//  * @example
+//  *
+//  *   [[ 'X',   ,'0' ],
+//  *    [    ,'X','0' ],       =>  'X'
+//  *    [    ,   ,'X' ]]
+//  *
+//  *   [[ '0','0','0' ],
+//  *    [    ,'X',    ],       =>  '0'
+//  *    [ 'X',   ,'X' ]]
+//  *
+//  *   [[ '0','X','0' ],
+//  *    [    ,'X',    ],       =>  undefined
+//  *    [ 'X','0','X' ]]
+//  *
+//  *   [[    ,   ,    ],
+//  *    [    ,   ,    ],       =>  undefined
+//  *    [    ,   ,    ]]
+//  *
+//  */
+// function evaluateTicTacToePosition( /* position */ ) {
+//   throw new Error('Not implemented');
+// }
 
 
 module.exports = {
@@ -406,15 +447,15 @@ module.exports = {
   isTriangle,
   doRectanglesOverlap,
   isInsideCircle,
-  findFirstSingleChar,
-  getIntervalString,
-  reverseString,
-  reverseInteger,
-  isCreditCardNumber,
-  getDigitalRoot,
-  isBracketsBalanced,
-  toNaryString,
-  getCommonDirectoryPath,
-  getMatrixProduct,
-  evaluateTicTacToePosition,
+  // findFirstSingleChar,
+  // getIntervalString,
+  // reverseString,
+  // reverseInteger,
+  // isCreditCardNumber,
+  // getDigitalRoot,
+  // isBracketsBalanced,
+  // toNaryString,
+  // getCommonDirectoryPath,
+  // getMatrixProduct,
+  // evaluateTicTacToePosition,
 };
